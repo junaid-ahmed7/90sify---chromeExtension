d17674d624669b47975eb94e11d09190edb4e968
Junaid Ahmed <junaid7ahmed96@gmail.com>
2023-02-16 08:56:09 -0500

    v1

--- remove.js
+++ remove.js
@@ -1,9 +1,23 @@
-const scripts = document.querySelectorAll('script');
+document.title = "GeoCities!";
+const body = document.querySelector("body");
+const image = document.createElement("iframe");
+image.setAttribute("id", "gif");
+image.src = "https://giphy.com/embed/3rgXBumyEL9086dy48";
+image.style = "width:200px;height:150px;frameBorder:0";
+body.prepend(image);
+const image2 = document.createElement("iframe");
+image2.setAttribute("id", "gif");
+image2.src = "https://giphy.com/embed/RxR1KghIie2iI";
+image2.style = "width:200px;height:150px;frameBorder:0";
+body.prepend(image2);
 
-scripts.forEach((script) => {
-  console.log(script.src);
-
-  script.src = '';
-
-  console.log(script.src);
-});
+//audio wont work :(
+// const audio = document.createElement("audio");
+// audio.src = "dialup.mp3";
+// audio.id = "music";
+// audio.crossOrigin="anonymous"
+// body.append(audio);
+// const audioelem = document.querySelector("#music");
+// body.addEventListener("click", () => {
+//   audioelem.play();
+// });
